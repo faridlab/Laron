46c909cfaf8866e601a4fc39df441f4290939a54
faridlab <e.faridhidayat@gmail.com>
2019-01-10 15:59:58 +0700

    navbar

--- resources/views/layouts/bootstrap.blade.php
+++ resources/views/layouts/bootstrap.blade.php
@@ -22,56 +22,11 @@
 </head>
 
 <body>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top flex-md-nowrap shadow">
-    <span class="navbar-brand mb-0 h1 col-md-2">{{ config('app.name', 'Laron') }}</span>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <form class="form-inline my-2 my-lg-0">
-        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      </form>
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
-            aria-haspopup="true" aria-expanded="false">
-            <span data-feather="settings"></span>
-          </a>
-          <div class="dropdown-menu shadow" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="#">Action</a>
-            <a class="dropdown-item" href="#">Another action</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">Something else here</a>
-          </div>
-        </li>
-      </ul>
-
-      <ul class="navbar-nav mr-sm-2">
-        <li class="nav-item">
-          <a class="nav-link" href="#"><span data-feather="mail"></span></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#"><span data-feather="bell"></span></a>
-        </li>
-        <li class="nav-item active dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbar-dropdown-profile" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Faridlab
-          </a>
-          <div class="dropdown-menu dropdown-menu-right shadow" aria-labelledby="navbar-dropdown-profile">
-            <a class="dropdown-item" href="#">Profile</a>
-            <a class="dropdown-item" href="#">Settings</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item text-danger" href="#">Logout</a>
-          </div>
-        </li>
-      </ul>
-    </div>
-  </nav>
-
   <div class="container-fluid">
+    <!-- FOOTER -->
+    @include('layouts.navbar')
     <div class="row">
+
       <nav class="col-md-2 d-none d-md-block bg-light sidebar">
         <div class="sidebar-sticky">
           <ul class="nav flex-column">
--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,218 +1,53 @@
-<nav class="header-navbar navbar-expand-md navbar navbar-with-menu fixed-top navbar-light navbar-hide-on-scroll navbar-border navbar-shadow navbar-brand-center">
-  <div class="navbar-wrapper">
-    <div class="navbar-header">
-      <ul class="nav navbar-nav flex-row">
-        <li class="nav-item mobile-menu d-md-none mr-auto"><a class="nav-link nav-menu-main menu-toggle hidden-xs" href="#"><i
-              class="ft-menu font-large-1"></i></a></li>
-        <li class="nav-item">
-          <a class="navbar-brand" href="">
-            <img class="brand-logo" alt="Laron" src="../../../app-assets/images/logo/logo-dark-sm.png">
-            <h3 class="brand-text">KOL</h3>
-          </a>
-        </li>
-        <li class="nav-item d-md-none">
-          <a class="nav-link open-navbar-container" data-toggle="collapse" data-target="#navbar-mobile"><i class="fa fa-ellipsis-v"></i></a>
-        </li>
-      </ul>
-    </div>
-    <div class="navbar-container content">
-      <div class="collapse navbar-collapse" id="navbar-mobile">
-        <ul class="nav navbar-nav mr-auto float-left">
-          <li class="nav-item d-none d-md-block"><a class="nav-link nav-menu-main menu-toggle hidden-xs" href="#"><i
-                class="ft-menu"> </i></a></li>
-          <li class="nav-item d-none d-md-block"><a class="nav-link nav-link-expand" href="#"><i class="ficon ft-maximize"></i></a></li>
-          <li class="nav-item nav-search"><a class="nav-link nav-link-search" href="#"><i class="ficon ft-search"></i></a>
-            <div class="search-input">
-              <input class="input" type="text" placeholder="Explore Robust...">
-            </div>
-          </li>
-        </ul>
-        <ul class="nav navbar-nav float-right">
-          <li class="dropdown dropdown-language nav-item"><a class="dropdown-toggle nav-link" id="dropdown-flag" href="#"
-              data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="flag-icon flag-icon-gb"></i><span>English</span><span
-                class="selected-language"></span></a>
-            <div class="dropdown-menu" aria-labelledby="dropdown-flag"><a class="dropdown-item" href="#"><i class="flag-icon flag-icon-gb"></i>
-                English</a>
-              <a class="dropdown-item" href="#"><i class="flag-icon flag-icon-fr"></i> French</a><a class="dropdown-item"
-                href="#"><i class="flag-icon flag-icon-cn"></i> Chinese</a><a class="dropdown-item" href="#"><i class="flag-icon flag-icon-de"></i>
-                German</a></div>
-          </li>
-          <li class="dropdown dropdown-notification nav-item">
-            <a class="nav-link nav-link-label" href="#" data-toggle="dropdown"><i class="ficon ft-bell"></i>
-              <span class="badge badge-pill badge-default badge-danger badge-default badge-up">5</span>
-            </a>
-            <ul class="dropdown-menu dropdown-menu-media dropdown-menu-right">
-              <li class="dropdown-menu-header">
-                <h6 class="dropdown-header m-0">
-                  <span class="grey darken-2">Notifications</span>
-                </h6>
-                <span class="notification-tag badge badge-default badge-danger float-right m-0">5 New</span>
-              </li>
-              <li class="scrollable-container media-list w-100">
-                <a href="javascript:void(0)">
-                  <div class="media">
-                    <div class="media-left align-self-center"><i class="ft-plus-square icon-bg-circle bg-cyan"></i></div>
-                    <div class="media-body">
-                      <h6 class="media-heading">You have new order!</h6>
-                      <p class="notification-text font-small-3 text-muted">Lorem ipsum dolor sit amet, consectetuer
-                        elit.</p>
-                      <small>
-                        <time class="media-meta text-muted" datetime="2015-06-11T18:29:20+08:00">30 minutes ago</time>
-                      </small>
-                    </div>
-                  </div>
-                </a>
-                <a href="javascript:void(0)">
-                  <div class="media">
-                    <div class="media-left align-self-center"><i class="ft-download-cloud icon-bg-circle bg-red bg-darken-1"></i></div>
-                    <div class="media-body">
-                      <h6 class="media-heading red darken-1">99% Server load</h6>
-                      <p class="notification-text font-small-3 text-muted">Aliquam tincidunt mauris eu risus.</p>
-                      <small>
-                        <time class="media-meta text-muted" datetime="2015-06-11T18:29:20+08:00">Five hour ago</time>
-                      </small>
-                    </div>
-                  </div>
-                </a>
-                <a href="javascript:void(0)">
-                  <div class="media">
-                    <div class="media-left align-self-center"><i class="ft-alert-triangle icon-bg-circle bg-yellow bg-darken-3"></i></div>
-                    <div class="media-body">
-                      <h6 class="media-heading yellow darken-3">Warning notifixation</h6>
-                      <p class="notification-text font-small-3 text-muted">Vestibulum auctor dapibus neque.</p>
-                      <small>
-                        <time class="media-meta text-muted" datetime="2015-06-11T18:29:20+08:00">Today</time>
-                      </small>
-                    </div>
-                  </div>
-                </a>
-                <a href="javascript:void(0)">
-                  <div class="media">
-                    <div class="media-left align-self-center"><i class="ft-check-circle icon-bg-circle bg-cyan"></i></div>
-                    <div class="media-body">
-                      <h6 class="media-heading">Complete the task</h6>
-                      <small>
-                        <time class="media-meta text-muted" datetime="2015-06-11T18:29:20+08:00">Last week</time>
-                      </small>
-                    </div>
-                  </div>
-                </a>
-                <a href="javascript:void(0)">
-                  <div class="media">
-                    <div class="media-left align-self-center"><i class="ft-file icon-bg-circle bg-teal"></i></div>
-                    <div class="media-body">
-                      <h6 class="media-heading">Generate monthly report</h6>
-                      <small>
-                        <time class="media-meta text-muted" datetime="2015-06-11T18:29:20+08:00">Last month</time>
-                      </small>
-                    </div>
-                  </div>
-                </a>
-              </li>
-              <li class="dropdown-menu-footer"><a class="dropdown-item text-muted text-center" href="javascript:void(0)">Read
-                  all notifications</a></li>
-            </ul>
-          </li>
-          <li class="dropdown dropdown-notification nav-item">
-            <a class="nav-link nav-link-label" href="#" data-toggle="dropdown"><i class="ficon ft-mail"></i>
-              <span class="badge badge-pill badge-default badge-info badge-default badge-up">5 </span>
-            </a>
-            <ul class="dropdown-menu dropdown-menu-media dropdown-menu-right">
-              <li class="dropdown-menu-header">
-                <h6 class="dropdown-header m-0">
-                  <span class="grey darken-2">Messages</span>
-                </h6>
-                <span class="notification-tag badge badge-default badge-warning float-right m-0">4 New</span>
-              </li>
-              <li class="scrollable-container media-list w-100">
-                <a href="javascript:void(0)">
-                  <div class="media">
-                    <div class="media-left">
-                      <span class="avatar avatar-sm avatar-online rounded-circle">
-                        <img src="../../../app-assets/images/portrait/small/avatar-s-19.png" alt="avatar"><i></i></span>
-                    </div>
-                    <div class="media-body">
-                      <h6 class="media-heading">Margaret Govan</h6>
-                      <p class="notification-text font-small-3 text-muted">I like your portfolio, let's start.</p>
-                      <small>
-                        <time class="media-meta text-muted" datetime="2015-06-11T18:29:20+08:00">Today</time>
-                      </small>
-                    </div>
-                  </div>
-                </a>
-                <a href="javascript:void(0)">
-                  <div class="media">
-                    <div class="media-left">
-                      <span class="avatar avatar-sm avatar-busy rounded-circle">
-                        <img src="../../../app-assets/images/portrait/small/avatar-s-2.png" alt="avatar"><i></i></span>
-                    </div>
-                    <div class="media-body">
-                      <h6 class="media-heading">Bret Lezama</h6>
-                      <p class="notification-text font-small-3 text-muted">I have seen your work, there is</p>
-                      <small>
-                        <time class="media-meta text-muted" datetime="2015-06-11T18:29:20+08:00">Tuesday</time>
-                      </small>
-                    </div>
-                  </div>
-                </a>
-                <a href="javascript:void(0)">
-                  <div class="media">
-                    <div class="media-left">
-                      <span class="avatar avatar-sm avatar-online rounded-circle">
-                        <img src="../../../app-assets/images/portrait/small/avatar-s-3.png" alt="avatar"><i></i></span>
-                    </div>
-                    <div class="media-body">
-                      <h6 class="media-heading">Carie Berra</h6>
-                      <p class="notification-text font-small-3 text-muted">Can we have call in this week ?</p>
-                      <small>
-                        <time class="media-meta text-muted" datetime="2015-06-11T18:29:20+08:00">Friday</time>
-                      </small>
-                    </div>
-                  </div>
-                </a>
-                <a href="javascript:void(0)">
-                  <div class="media">
-                    <div class="media-left">
-                      <span class="avatar avatar-sm avatar-away rounded-circle">
-                        <img src="../../../app-assets/images/portrait/small/avatar-s-6.png" alt="avatar"><i></i></span>
-                    </div>
-                    <div class="media-body">
-                      <h6 class="media-heading">Eric Alsobrook</h6>
-                      <p class="notification-text font-small-3 text-muted">We have project party this saturday.</p>
-                      <small>
-                        <time class="media-meta text-muted" datetime="2015-06-11T18:29:20+08:00">last month</time>
-                      </small>
-                    </div>
-                  </div>
-                </a>
-              </li>
-              <li class="dropdown-menu-footer"><a class="dropdown-item text-muted text-center" href="javascript:void(0)">Read
-                  all messages</a></li>
-            </ul>
-          </li>
-          <li class="dropdown dropdown-user nav-item">
-            <a class="dropdown-toggle nav-link dropdown-user-link" href="#" data-toggle="dropdown">
-              <span class="avatar avatar-online">
-                <img src="../../../app-assets/images/portrait/small/avatar-s-1.png" alt="avatar"><i></i></span>
-              <span class="user-name">John Doe</span>
-            </a>
-            <div class="dropdown-menu dropdown-menu-right"><a class="dropdown-item" href="#"><i class="ft-user"></i>
-                Edit Profile</a>
-              <a class="dropdown-item" href="#"><i class="ft-mail"></i> My Inbox</a>
-              <a class="dropdown-item" href="#"><i class="ft-check-square"></i> Task</a><a class="dropdown-item" href="#"><i
-                  class="ft-message-square"></i> Chats</a>
-              <div class="dropdown-divider"></div>
-              <a class="dropdown-item" href="{{ route('logout') }}"
+<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top flex-md-nowrap shadow">
+  <span class="navbar-brand mb-0 h1 col-md-2">{{ config('app.name', 'Laron') }}</span>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+    </form>
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+          aria-haspopup="true" aria-expanded="false">
+          <span data-feather="settings"></span>
+        </a>
+        <div class="dropdown-menu shadow" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+    </ul>
+
+    <ul class="navbar-nav mr-sm-2">
+      <li class="nav-item">
+        <a class="nav-link" href="#"><span data-feather="mail"></span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"><span data-feather="bell"></span></a>
+      </li>
+      <li class="nav-item active dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbar-dropdown-profile" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Faridlab
+        </a>
+        <div class="dropdown-menu dropdown-menu-right shadow" aria-labelledby="navbar-dropdown-profile">
+          <a class="dropdown-item" href="#">Profile</a>
+          <a class="dropdown-item" href="#">Settings</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item text-danger" text-danger href="{{ route('logout') }}"
                 onclick="event.preventDefault();
                 document.getElementById('logout-form').submit();">
                 <i class="ft-power"></i> {{ __('Logout') }}</a>
               <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                   @csrf
               </form>
-            </div>
-          </li>
-        </ul>
-      </div>
-    </div>
+        </div>
+      </li>
+    </ul>
   </div>
 </nav>
